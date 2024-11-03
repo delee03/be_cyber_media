877cdd11963a85aabe38212c4e3589e6d500dae0
Pham Phat <phamphat2003vn@gmail.com>
2024-11-03 13:52:22 +0700

    add authen brcrypt hash

--- src/common/prisma/init.prisma.js
+++ src/common/prisma/init.prisma.js
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    omit: {
+        users: {
+            pass_word: true,
+        },
+    },
+});
 
 export default prisma;
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,37 @@
+import { BadRequestError } from "../common/helpers/error.helper.js";
+import prisma from "../common/prisma/init.prisma.js";
+import bcrypt from "bcrypt";
+
 const authService = {
     register: async (req) => {
-        return `register`;
+        //b1: nhận dữ liệu từ client FE gửi lên
+        const { email, password, fullName } = req.body;
+        console.log(req.body);
+        //b2: kiểm tra dữ liệu từ client FE gửi lên có hợp lệ không
+        if (!email || !password || !fullName) {
+            throw new BadRequestError("Email, password, fullName is invalid");
+        }
+        //b3: kiểm tra email đã tồn tại trong db chưa
+        //findunique dành cho 1 index duy nhất hoặc primary key nếu k dùng findFirst
+        const user = await prisma.users.findFirst({
+            where: {
+                email: email,
+            },
+        });
+        if (user) {
+            throw new BadRequestError("Email is exist");
+        }
+        console.log(req.body);
+        //b4:  mã hóa password 1 chiều trước khi lưu vào db, tạo user trong db
+        const hashPassword = bcrypt.hashSync(password, 10);
+        const newUser = await prisma.users.create({
+            data: {
+                email: email,
+                pass_word: hashPassword,
+                full_name: fullName,
+            },
+        });
+        return newUser;
     },
 };
 
