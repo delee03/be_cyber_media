292022fb0fbbb3aa4d669270f5b27cd2d003e23c
Pham Phat <phamphat2003vn@gmail.com>
2024-12-15 17:12:35 +0700

    change CI file

--- src/common/sequelize/connect.sequelize.js
+++ src/common/sequelize/connect.sequelize.js
@@ -1,21 +1,21 @@
-import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(`db_media`, `root`, `123`, {
-    host: `localhost`,
-    port: `3305`,
-    dialect: `mysql`,
-    // logging: false
-});
+// const sequelize = new Sequelize(`db_media`, `root`, `123`, {
+//     host: `localhost`,
+//     port: `3305`,
+//     dialect: `mysql`,
+//     // logging: false
+// });
 
-// Kiểm tra kết nối
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log(`Kết nối db thành công`);
-    })
-    .catch((err) => {
-        console.log(err);
-        console.log(`Kết nối db không thành công`);
-    });
+// // Kiểm tra kết nối
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log(`Kết nối db thành công`);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//         console.log(`Kết nối db không thành công`);
+//     });
 
-export default sequelize;
+// export default sequelize;
