dff64a19bb9340e432e3b664f23137c062ca8265
Pham Phat <phamphat2003vn@gmail.com>
2024-12-16 12:25:30 +0700

    feat: jobs 10

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -17,7 +17,7 @@ const videoController = {
             const result = await videoService.listVideo(req);
             const resData = responseSuccess(
                 result,
-                "Trả dữ liệu thành công 99999",
+                "Trả dữ liệu thành công Phát đẹp zai",
                 200
             );
             res.status(resData.code).json(resData);
